6e4337911a1a9005e7dd7ff7495d8265938be537
KelechiOkoroafor <okoroaforkelechi123@gmail.com>
2022-05-05 13:45:26 +0100

    fixed buy and sell

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
-import Designs from "./components/Designs"
-// import Button from "./components/Button";
-
+import React, { useEffect } from "react";
+import Designs from "./components/Designs";
+import Blogs from "./components/Designs";
+import BuySell from "./components/BuySell";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
+import scrollreveal from "scrollreveal";
 
 
 
 export default function App() {
-  return (
-   <div>
-     <Designs/>
-       {/*<Button />*/}
-   </div>
+  useEffect(() => {
+      const registerAnimations = () => {
+          const sr = scrollreveal({
+              origin: "buttom",
+              distance: "80px",
+              duration: 2000,
+              reset: false,
+          });
+          sr.reveal(
+              `
+              nav, 
+              #home, 
+              #buysell1, 
+              #buysell2,
+              #buysell3,
+              #blogs, 
+              footer      
+              `,
+              {
+                  opacity: 0,
+                  interval: 500,
+              }
+          );
+      };
+
+      registerAnimations();
+  }, []);
+
+  window.setTimeout(() => {
+      const home = document.getElementById("home");
+      home.style.transform = "home";
+  }, 1500);
+
+  return(
+      <div>
+          <ScrollToTop />
+          <Navbar />
+          <Home />
+          <Designs />
+          <BuySell />
+          <Blogs />
+          <Footer />
+      </div>
   );
 }
